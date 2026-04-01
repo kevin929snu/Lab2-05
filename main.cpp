25966b21ed074ade8972bd1094856b7fd743deb8
mungk <kevin929@snu.ac.kr>
2026-04-01 17:21:54 +0900

    modify func 이영학

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
+    int start;
+    if (vec.empty()) start = 1;
+    else start = vec.back() + 1;
 
-
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
